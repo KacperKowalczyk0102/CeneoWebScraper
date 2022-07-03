d331edc0bb667290a6b4ffdbcc57751551720b93
KacperKowalczyk0102 <92937759+KacperKowalczyk0102@users.noreply.github.com>
2022-07-03 22:25:12 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
+import os
+from distutils import extension
+from matplotlib.style import library
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from app.models.product import 
+import markdown
 
 @app.route('/')
 def index():
@@ -9,14 +14,32 @@ def index():
 def author():
     return render_template("author.html.jinja")
 
-@app.route('/extract/<product_id>')
-def  extract(product_id):
-    pass
+@app.route('/extract', methods=["POST", "GET"])
+def extract():
+    if request.method == "POST":
+        product_id = request.form.get("product_id")
+        product = Product(product_id)
+        product.extract_product().process_stats().draw_charts()
+        # product.save_stats()
+        product.save_opinions()
+        return redirect(url_for("product", product_id=product_id))
+    else:
+        return render_template("extract.html.jinja")
 
 @app.route('/products')
-def  products():
-    pass
+def products():
+    products = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
+    return render_template("products.html.jinja", products=products)
+
+def read_me_plik():
+    with open("README.md", "r") as mdfile:
+        readme_html=md.markdown(mdfile.read(), extensions=['markdown.extensions.tables', 'markdown.extensons.fanced_code'] )
+    return render_template("index.html.jinja", libraries=libraries, readme_html=readme_html )
 
 @app.route('/product/<product_id>')
-def  product(product_id):
-    pass
+def product(product_id):
+    product = Product(product_id)
+    product.read_from_json()
+    opinions = product.opinions_do_df()
+    stats = product.stats_to_dict()
+    return render_template("product.html.jinja", stats=stats, product_id=product_id, opinions=opinions)
